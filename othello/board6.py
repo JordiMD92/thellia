8e8ddfa04fabc28fc42c7313e6e6c3804afc8881
xordi <jordi.merino@estudiants.urv.cat>
2017-12-09 18:48:20 +0100

    Bug fixes, Erased 6x6 board, batch trainning

---
+++ othello/board6.py
@@ -0,0 +1,151 @@
+import numpy
+from copy import deepcopy
+
+class Board(object):
+    BLACK = 1
+    WHITE = -1
+
+    FITBOARD = [[99,-8,6,6,-8,99],
+                [-8,-24,-3,-3,-24,-8],
+                [6,-3,0,0,-3,6],
+                [6,-3,0,0,-3,6],
+                [-8,-24,-3,-3,-24,-8],
+                [99,-8,6,6,-8,99]]
+    SIZE = 6
+
+    def __init__(self):
+        self.board = numpy.zeros((Board.SIZE,Board.SIZE), int)
+        self.board[2][2] = Board.WHITE
+        self.board[3][3] = Board.WHITE
+        self.board[2][3] = Board.BLACK
+        self.board[3][2] = Board.BLACK
+
+        self.remaining_pieces = Board.SIZE*Board.SIZE-4
+        self.passCount = 0
+        self.score = {Board.BLACK: 2, Board.WHITE: 2}
+
+    def getScore(self):
+        """ Returns actual score """
+        return self.score
+
+    def getBoard(self):
+        """ Returns actual state of board """
+        return self.board
+
+    def getBoardShape(self,num_positions,tile):
+        """ Returns actual state of board in 1 Dimension of #input positions
+        @param int num_positions
+        @param int tile
+        @return list(int) shapedBoard
+        """
+        oneD = self.board.reshape((pow(Board.SIZE,2)))
+        if num_positions == pow(Board.SIZE,2):
+            # vector of 36 positions
+            return oneD
+        if num_positions == (pow(Board.SIZE,2)*2+1):
+            # Half for black pieces positions, other half for white positions, last position = tile
+            shapedBoard = numpy.zeros((pow(Board.SIZE,2)*2+1),int)
+            idx = 0
+            for pos in oneD[0]:
+                if pos == 0:
+                    shapedBoard[idx] = 0
+                    shapedBoard[idx+pow(Board.SIZE)] = 0
+                elif pos == self.BLACK:
+                    shapedBoard[idx] = 1
+                else:
+                    shapedBoard[idx+pow(Board.SIZE,2)] = 1
+                idx += 1
+
+            shapedBoard[(pow(Board.SIZE,2)*2)] = (1+tile)/2
+            return shapedBoard.reshape((-1,(pow(Board.SIZE,2)*2+1)))
+
+    def isOnBoard(self,c,r):
+        """ Returns true if valid position or false otherwise """
+        return (c>=0) and (c<=Board.SIZE-1) and (r>=0) and (r<=Board.SIZE-1)
+
+    def isEndGame(self):
+        if self.remaining_pieces == 0 or self.passCount == 2:
+            return True
+        return False
+
+    def next(self,tile,action):
+        """
+        Update board and return new board and reward
+        @param int tile
+        @param int action
+        @return Board,int,bool sPrime,reward,done
+        """
+        sP = deepcopy(self)
+        sP.updateBoard(tile,action)
+        reward = 0
+        if sP.isEndGame():
+            reward = -1
+            if sP.getScore()[tile] > sP.getScore()[-tile]:
+                reward = 1
+        return sP,reward, sP.isEndGame()
+
+    def updateBoard(self,tile,move):
+        """
+        Updates board with new movement if is a valid move
+        @param int tile
+            1 for BLACK
+            -1 for WHITE
+        @param int move
+            0-35 vector position
+        @return bool can_move
+        """
+        row = move // Board.SIZE
+        col = move % Board.SIZE
+        moves = self.isValidMove(tile,col,row)
+        if moves:
+            self.board[col][row] = tile
+            for position in moves:
+                self.board[position[0]][position[1]] = tile
+            self.score[tile] += len(moves) + 1
+            self.score[-tile] -= len(moves)
+            self.remaining_pieces -= 1
+            return True
+        else:
+            return False
+
+    def isValidMove(self,tile,col,row):
+        """
+        Checks if it is a valid move
+        @param int tile
+            1 for BLACK
+            -1 for WHITE
+        @param int col
+            0-Board.SIZE-1 column position
+        @param int row
+            0-Board.SIZE-1 row position
+        @return int[][]
+            void if invalid move
+            list of tiles to flip if valid move
+        """
+        flipTiles = []
+        if self.board[col][row] != 0:
+            return flipTiles
+
+        for dirCol in ((-1),(0),(1)):
+            for dirRow in ((-1),(0),(1)):
+                if dirCol == dirRow and dirCol == 0:
+                    continue
+                c = dirCol + col
+                r = dirRow + row
+                while self.isOnBoard(c,r) and self.board[c][r] != 0:
+                    if self.board[c][r] == tile:
+                        while True:
+                            c -= dirCol
+                            r -= dirRow
+                            if c == col and r == row:
+                                break
+                            flipTiles.append([c,r])
+                    if self.board[c][r] + tile == 0:
+                        c += dirCol
+                        r += dirRow
+        return flipTiles
+
+    def get_fit_value(self,move):
+        row = move // Board.SIZE
+        col = move % Board.SIZE
+        return Board.FITBOARD[row][col]
