d8e250fa9ec738a74e44bd39ebdf0d00854afcdf
Jordi Merino <xo0rdii@gmail.com>
2017-12-27 17:48:52 +0000

    Added trained models

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
-python main.py -m load -e 100000 -n sigmoid -r 0.005;
-python main.py -m load -e 100000 -n relu -r 0.005;
-python main.py -m load -e 100000 -n tanh -r 0.005;
+python main.py -m train -e 100000 -n sigmoid -r 0.005 -b QP -w MP;
+python main.py -m train -e 100000 -n relu -r 0.005 -b QP -w MP;
+python main.py -m train -e 100000 -n tanh -r 0.005 -b QP -w MP;
