681bf8f31f404d291ccdc4f01ab5ce0dca0e9a0c
Jordi Merino <xo0rdii@gmail.com>
2017-12-28 17:55:08 +0000

    Added DDQN Tensorflow models

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
-python main.py -m train -e 100000 -n sigmoid -r 0.005 -b QP -w MP;
-python main.py -m train -e 100000 -n relu -r 0.005 -b QP -w MP;
-python main.py -m train -e 100000 -n tanh -r 0.005 -b QP -w MP;
+python main.py -m load -e 100000 -n relu -r 0.0001 -b QP -w MP;
+python main.py -m train -e 100000 -n relu -r 0.0001 -b QP -w MP;
+python main.py -m load -e 100000 -n sigmoid -r 0.0001 -b QP -w MP;
+python main.py -m train -e 100000 -n sigmoid -r 0.0001 -b QP -w MP;
