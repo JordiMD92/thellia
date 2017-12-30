11d482922417a0e15778d7a6ae9a3161d85f6d72
Jordi Merino <xo0rdii@gmail.com>
2017-12-30 16:59:34 +0000

    Added new relu models

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
-python main.py -m load -e 100000 -n relu -r 0.0001 -b QP -w MP -d 0.4;
-python main.py -m train -e 100000 -n relu -r 0.0001 -b QP -w MP -d 0.4;
-python main.py -m train -e 480000 -n relu -r 0.0001 -b QP -w MP -d 0.4;
+python main.py -m load -e 100000 -n relu -b QP -w MP -r 0.0001 -d 0.4;
+python main.py -m load -e 100000 -n reluLarge -b QP -w MP -r 0.0001 -d 0.4;
+python main.py -m train -e 100000 -n relu -b QP -w MP -r 0.0001 -d 0.4;
+python main.py -m train -e 100000 -n reluLarge -b QP -w MP -r 0.0001 -d 0.4;
