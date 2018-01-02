3a8a5ea7e413d25283a64a9be51b186fc80a025e
Jordi Merino <xo0rdii@gmail.com>
2018-01-02 10:23:52 +0000

    Models without drop

--- script.sh
+++ script.sh
@@ -1,6 +1,4 @@
-python main.py -m load -e 100000 -n relu -b QP -w MP -r 0.0001 -d 0.4;
-python main.py -m load -e 100000 -n reluLarge -b QP -w MP -r 0.0001 -d 0.4;
-python main.py -m train -e 100000 -n relu -b QP -w MP -r 0.0001 -d 0.4;
-python main.py -m train -e 100000 -n reluLarge -b QP -w MP -r 0.0001 -d 0.4;
-python main.py -m train -e 100000 -n sigmoid -b QP -w MP -r 0.0001;
-python main.py -m train -e 100000 -n tanh -b QP -w MP -r 0.0001;
+python main.py -m load -e 100000 -n relu -b QP -w MP -r 0.0001 -d 1.0;
+python main.py -m load -e 100000 -n reluLarge -b QP -w MP -r 0.0001 -d 1.0;
+python main.py -m train -e 100000 -n relu -b QP -w MP -r 0.0001 -d 1.0;
+python main.py -m train -e 100000 -n reluLarge -b QP -w MP -r 0.0001 -d 1.0;
