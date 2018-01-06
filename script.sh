f0140c7f32b91529aa0b27995a0a6b7027c87cde
Jordi Merino <xo0rdii@gmail.com>
2018-01-06 10:14:29 +0000

    Models with 129 input layer

--- script.sh
+++ script.sh
@@ -1,4 +1,3 @@
-python main.py -m train -e 100000 -n reluDuel -b QP -w MP -r 0.0001 -d 0.4;
-python main.py -m train -e 100000 -n reluLargeDuel -b QP -w MP -r 0.0001 -d 0.4;
-python main.py -m train -e 100000 -n reluDuel -b QP -w RP -r 0.0001 -d 0.4;
-python main.py -m train -e 100000 -n reluLargeDuel -b QP -w RP -r 0.0001 -d 0.4;
+python main.py -m train -e 100000 -n reluDuel129 -b QP -w MP -r 0.0001 -d 0.4;
+python main.py -m train -e 100000 -n reluDuel129 -b QP -w RP -r 0.0001 -d 0.4;
+python main.py -m train -e 100000 -n reluDuel129 -b QP -w QP -r 0.0001 -d 0.4;
