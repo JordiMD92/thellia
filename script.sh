499782b9cb47270577f11e89c6bdea3600b3397a
Jordi Merino <xo0rdii@gmail.com>
2018-01-09 17:04:45 +0000

    More models

--- script.sh
+++ script.sh
@@ -1,2 +1,1 @@
-python main.py -m train -e 100000 -n reluDuel129 -b QP -w QP -r 0.0001 -d 0.4 --load train_100k_reluDuel129_bQP_wMP_lr0.0001_drop0.4_mm;
-python main.py -m train -e 500000 -n reluDuel129 -b QP -w QP -r 0.0001 -d 0.4;
+python main.py -m train -e 100000 -n reluDuel129 -b QP -w QP;
