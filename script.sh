166fa5179770c01bbaab5b4eb082a8f9e599f5d8
Jordi Merino <xo0rdii@gmail.com>
2018-01-11 13:15:41 +0000

    Model with 0,6 dropout

--- script.sh
+++ script.sh
@@ -1,1 +1,2 @@
-python main.py -m train -e 100000 -n reluDuel129 -b QP -w MP;
+python main.py -m train -e 100000 -n reluDuel129 -b QP -w MP -d 0.2;
+python main.py -m train -e 100000 -n reluDuel129 -b QP -w MP -d 0.6;
